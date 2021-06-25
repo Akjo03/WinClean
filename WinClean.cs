7e3aacc040042d4181e68b8ed3c49084f67f1fed
Lukas Freckmann <lukas.freckmann@gmx.ch>
2021-06-25 21:50:58 +0200

    Fixes #10 | Added localization support

--- WinClean.cs
+++ WinClean.cs
@@ -1,6 +1,8 @@
 ﻿using WinClean.resources;
 
 using System;
+using System.Globalization;
+using System.Threading;
 
 namespace WinClean {
     /// <summary>
@@ -18,6 +20,7 @@ namespace WinClean {
         /// </summary>
         /// <param name="args">Command line arguments</param>
         public static void Main(string[] args) {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-us");
             Console.WriteLine(Strings.WelcomeMessage);
             Console.WriteLine(Strings.EarlyDevelopmentMessage);
             Console.ReadLine();
