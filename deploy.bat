325a135fe844912fbbc66b65690cc557510dbe49
Lukas Freckmann <lukas.freckmann@gmx.ch>
2021-06-26 16:13:16 +0200

    Updated deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -4,7 +4,13 @@ echo Deplyoing and publishing WinClean... please wait!
 set runtime=win-x64
 set version=0.0.0
 
-dotnet publish -c Release --framework netcoreapp3.1 --runtime %runtime% --nologo -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -v q --version-suffix %version%
+dotnet publish WinClean.csproj -c Release --framework netcoreapp3.1 --runtime %runtime% --nologo -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -v q --version-suffix %version%
+if errorlevel 1 goto error
 echo Finished deploying WinClean!
+goto end
+:error
+echo [91mError on deploying WinClean![0m
+goto end
+:end
 pause.
 exit
